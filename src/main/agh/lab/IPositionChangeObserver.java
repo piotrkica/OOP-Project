8fc9f167b3e33e9df0d84fb495df8ff10069e1ca
Piotrek <piotr.kica.35@gmail.com>
2020-11-25 18:19:19 +0100

    newPositions->oldPosition

--- src/main/agh/lab/IPositionChangeObserver.java
+++ src/main/agh/lab/IPositionChangeObserver.java
@@ -1,5 +1,5 @@
 package agh.lab;
 
 public interface IPositionChangeObserver {
-    void positionChanged(Animal animal, Vector2d newPosition);
+    void positionChanged(Animal animal, Vector2d oldPositions);
 }
