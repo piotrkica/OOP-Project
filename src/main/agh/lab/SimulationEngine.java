83b875ae3aeb1492ccd456765adfdd36cc2b1d63
Piotrek <piotr.kica.35@gmail.com>
2020-11-25 21:36:33 +0100

    adjusting constructor to multimap usage

--- src/main/agh/lab/SimulationEngine.java
+++ src/main/agh/lab/SimulationEngine.java
@@ -5,12 +5,12 @@ import java.util.Collection;
 public class SimulationEngine implements IEngine {
     private final MoveDirection[] directions;
     private final MapWithJungle map;
-    private final Vector2d[] animalPositions;
+    private final int animalsNo;
 
     public SimulationEngine(MoveDirection[] directions, MapWithJungle map, Vector2d[] startingPosition) {
         this.directions = directions;
         this.map = map;
-        this.animalPositions = startingPosition;
+        this.animalsNo = startingPosition.length;
         for (Vector2d position : startingPosition) {
             map.place(new Animal(map, position));
         }
@@ -21,7 +21,7 @@ public class SimulationEngine implements IEngine {
         Animal[] animals = new Animal[animalCollection.size()];
         animals = animalCollection.toArray(animals);
         for (int i = 0; i < directions.length; i++) {
-            animals[i % animalPositions.length].move(directions[i]);
+            animals[i % animalsNo].move(directions[i]);
         }
 
     }
