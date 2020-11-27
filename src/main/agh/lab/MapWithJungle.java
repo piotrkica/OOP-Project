2196131db2b79989a907213fac10609f49898bd5
Piotrek <piotr.kica.35@gmail.com>
2020-11-27 17:08:39 +0100

    moved methods to S.engine

--- src/main/agh/lab/MapWithJungle.java
+++ src/main/agh/lab/MapWithJungle.java
@@ -4,8 +4,6 @@ import java.util.*;
 import com.google.common.collect.ArrayListMultimap;
 import com.google.common.collect.Multimap;
 
-import static agh.lab.SimulationEngine.grassEnergyValue;
-import static agh.lab.SimulationEngine.startingEnergy;
 import static java.lang.Math.*;
 
 public class MapWithJungle implements IWorldMap, IPositionChangeObserver{
@@ -16,7 +14,6 @@ public class MapWithJungle implements IWorldMap, IPositionChangeObserver{
     private final Vector2d topRightJungle;
     private final Multimap<Vector2d, Animal> animalsMM = ArrayListMultimap.create();
     private final Map<Vector2d, Grass> grassTilesHM = new HashMap<>();
-    private final List<Animal> deadAnimals = new ArrayList<>();
     private final Random rand = new Random();
 
     public MapWithJungle(int width, int height, double jungleRatio, int grassTilesStartNo, int animalsNo) {
@@ -59,11 +56,28 @@ public class MapWithJungle implements IWorldMap, IPositionChangeObserver{
         return true;
     }
 
-    public void placeStartingAnimals(int animalsNo){
-        for (int i = 0; i < animalsNo; i++){
-            Vector2d position = findFreeTile(bottomLeftMap,topRightMap);
-            place(new Animal(this, position));
+    @Override
+    public boolean canMoveTo(Vector2d position) {
+        return position.follows(bottomLeftMap) && position.precedes(topRightMap);
+    }
+
+    @Override
+    public Object objectAt(Vector2d position) {
+        if (animalsMM.get(position).size() == 0){
+            return grassTilesHM.get(position);
         }
+        return animalsMM.get(position);
+    }
+
+    @Override
+    public void positionChanged(Animal animal, Vector2d oldPosition) {
+        animalsMM.remove(oldPosition, animal);
+        animalsMM.put(animal.getPosition(), animal);
+
+    }
+
+    public boolean isInJungle(Vector2d position){
+        return position.follows(bottomLeftJungle) && position.precedes(topRightJungle);
     }
 
     public Vector2d findFreeTile(Vector2d bottomLeft, Vector2d topRight){// potrzebny fix by się nie zapętlił jak nie ma wolnych miejsc
@@ -75,9 +89,40 @@ public class MapWithJungle implements IWorldMap, IPositionChangeObserver{
         return position;
     }
 
-    @Override
-    public boolean canMoveTo(Vector2d position) {
-        return position.follows(bottomLeftMap) && position.precedes(topRightMap);
+    public void placeStartingAnimals(int animalsNo){
+        for (int i = 0; i < animalsNo; i++){
+            Vector2d position = findFreeTile(bottomLeftMap,topRightMap);
+            place(new Animal(this, position));
+        }
+    }
+
+    public void placeGrassInJungle(){
+        Vector2d position;
+        position = findFreeTile(bottomLeftJungle, topRightJungle);
+        grassTilesHM.put(position, new Grass(position));
+    }
+
+    public void placeGrassOutsideJungle(){
+        Vector2d position;
+        do {
+            position = findFreeTile(bottomLeftMap,topRightMap);
+        } while(isInJungle(position));
+
+        grassTilesHM.put(position, new Grass(position));
+    }
+
+    public Vector2d findFreePositionForChild(Vector2d parentsPosition){ // na obecną chwile przeszukuje w promieniu 2 pól, może znaleźć miejsc po drugiej stronie mapy
+        for(int r = 1; r < 2; r++){
+            for(int i = -r; i < r; i++){
+                for(int j = -r; j < r; j++){
+                    Vector2d possiblePosition = repositionIfOutOfBounds(new Vector2d(i, j));
+                    if (!isOccupied(possiblePosition)){
+                        return possiblePosition;
+                    }
+                }
+            }
+        }
+        return parentsPosition;
     }
 
     public Vector2d repositionIfOutOfBounds(Vector2d position){
@@ -101,135 +146,16 @@ public class MapWithJungle implements IWorldMap, IPositionChangeObserver{
         return position;
     }
 
-    @Override
-    public Object objectAt(Vector2d position) {
-        if (animalsMM.get(position).size() == 0){
-            return grassTilesHM.get(position);
-        }
-        return animalsMM.get(position);
-    }
-
     public Multimap<Vector2d, Animal> getAnimalsMM() {
         return animalsMM;
     }
 
-    @Override
-    public void positionChanged(Animal animal, Vector2d oldPosition) {
-        animalsMM.remove(oldPosition, animal);
-        animalsMM.put(animal.getPosition(), animal);
-
+    public Map<Vector2d, Grass> getGrassTilesHM(){
+        return grassTilesHM;
     }
 
-    public boolean isInJungle(Vector2d position){
-        return position.follows(bottomLeftJungle) && position.precedes(topRightJungle);
-    }
 
-    public void placeGrassOutsideJungle(){
-        Vector2d position;
-        do {
-            position = findFreeTile(bottomLeftMap,topRightMap);
-        } while(isInJungle(position));
 
-        grassTilesHM.put(position, new Grass(position));
-    }
 
-    public void placeGrassInJungle(){
-        Vector2d position;
-        position = findFreeTile(bottomLeftJungle, topRightJungle);
-        grassTilesHM.put(position, new Grass(position));
-    }
 
-    public List<Animal> getStrongestAnimals(Vector2d position){
-        List<Animal> strongestAnimals = new LinkedList<>();
-        int strongestEnergy = 0;
-        for (Animal animal : animalsMM.get(position)) {
-            strongestEnergy = max(strongestEnergy, animal.getEnergy());
-        }
-        for (Animal animal : animalsMM.get(position)) {
-            if (animal.getEnergy() == strongestEnergy) {
-                strongestAnimals.add(animal);
-            }
-        }
-        return strongestAnimals;
-    }
-
-    public void eatGrass(){
-        Vector2d[] grassTiles = grassTilesHM.keySet().toArray(new Vector2d[0]);
-        for(Vector2d grassPosition : grassTiles){
-            if (animalsMM.get(grassPosition).size() >= 1){
-                grassTilesHM.remove(grassPosition);
-                List<Animal> strongestAnimals = getStrongestAnimals(grassPosition);
-                int grassEnergyValueSplit = floorDiv(grassEnergyValue, strongestAnimals.size());
-                for(Animal animal : strongestAnimals){
-                    animal.addEnergy(grassEnergyValueSplit);
-                }
-            }
-        }
-    }
-
-    public void removeDead() {
-        Collection<Animal> animalCollection = animalsMM.values();
-        Animal[] animals = new Animal[animalCollection.size()];
-        animals = animalCollection.toArray(animals);
-
-        for(Animal animal : animals){
-            if(animal.getEnergy() <= 0){
-                deadAnimals.add(animal);
-                animal.removeAllObservers();
-                animalsMM.remove(animal.getPosition(), animal);
-            }
-        }
-    }
-
-    public Vector2d findFreePositionForChild(Vector2d parentsPosition){ // na obecną chwile przeszukuje w promieniu 2 pól, może znaleźć miejsc po drugiej stronie mapy
-        for(int r = 1; r < 2; r++){
-            for(int i = -r; i < r; i++){
-                for(int j = -r; j < r; j++){
-                    Vector2d possiblePosition = repositionIfOutOfBounds(new Vector2d(i, j));
-                    if (!isOccupied(possiblePosition)){
-                        return possiblePosition;
-                    }
-                }
-            }
-        }
-        return parentsPosition;
-    }
-
-    public void reproduceIfPossible(){
-        Vector2d[] animalPositions = animalsMM.keySet().toArray(new Vector2d[0]); //animalsMM.keySet().size()
-        for (Vector2d position : animalPositions){
-            if (animalsMM.get(position).size() >= 2){
-                Animal strongest1 = null;
-                Animal strongest2 = null;
-                for (Animal animal : animalsMM.get(position)){
-                    if (strongest1 == null){
-                        strongest1 = animal;
-                    }
-                    else if (strongest2 == null){
-                        if (strongest1.getEnergy() > animal.getEnergy()){
-                            strongest2 = animal;
-                        }
-                        else{
-                            strongest2 = strongest1;
-                            strongest1 = animal;
-                        }
-                    }
-                    else if (strongest1.getEnergy() < animal.getEnergy()){
-                        strongest2 = strongest1;
-                        strongest1 = animal;
-                    }
-                    else if (strongest2.getEnergy() < animal.getEnergy()){
-                        strongest2 = animal;
-                    }
-                }
-                if (strongest1.getEnergy() > startingEnergy/2 && strongest2.getEnergy() > startingEnergy/2){
-                    int childEnergy = strongest1.getEnergy()/2 + strongest2.getEnergy()/2;
-                    strongest1.setEnergy(strongest1.getEnergy()/2);
-                    strongest2.setEnergy(strongest2.getEnergy()/2);
-                    strongest1.increaseChildNo();
-                    place(new Animal(this, findFreePositionForChild(position), strongest1.getGenes(),strongest2.getGenes(), childEnergy));
-                }
-            }
-        }
-    }
 }
--- src/main/agh/lab/SimulationEngine.java
+++ src/main/agh/lab/SimulationEngine.java
@@ -1,12 +1,17 @@
 package agh.lab;
 
-import java.util.Collection;
+import com.google.common.collect.Multimap;
+
+import java.util.*;
+
+import static java.lang.Math.max;
 
 public class SimulationEngine implements IEngine {
     private final int days;
     private final MapWithJungle map;
     public static int startingEnergy = 15;
     public static int grassEnergyValue = 10;
+    private final List<Animal> deadAnimals = new ArrayList<>();
 
 
     public SimulationEngine(int days, MapWithJungle map) {
@@ -16,24 +21,118 @@ public class SimulationEngine implements IEngine {
 
     public void run() {
         for (int i = 0; i < days;i++) {
-            Collection<Animal> animalCollection = map.getAnimalsMM().values();
-            Animal[] animals = new Animal[animalCollection.size()];
-            animals = animalCollection.toArray(animals);
-
             System.out.println(map);
-
-            this.map.removeDead();
-            for (Animal animal : animals) {
-                animal.move();
-            }
-            this.map.eatGrass();
-            this.map.reproduceIfPossible();
-            this.map.placeGrassInJungle();
-            this.map.placeGrassOutsideJungle();
-            for (Animal animal : animals) {
-                System.out.println(animal.getEnergy());
-            }
+            removeDead();
+            moveAnimals();
+            eatGrass();
+            reproduceIfPossible();
+            placeGrassInJungleAndOutside();
         }
         System.out.println(map);
     }
+
+    public void moveAnimals(){
+        Collection<Animal> animalCollection = map.getAnimalsMM().values();
+        Animal[] animals = new Animal[animalCollection.size()];
+        animals = animalCollection.toArray(animals);
+        for (Animal animal : animals) {
+            animal.move();
+        }
+        for (Animal animal : animals) {
+            System.out.println(animal.getEnergy());
+        }
+    }
+
+    public void placeGrassInJungleAndOutside(){
+        this.map.placeGrassInJungle();
+        this.map.placeGrassOutsideJungle();
+    }
+
+    public List<Animal> getStrongestAnimals(Vector2d position){
+        Multimap<Vector2d, Animal> animalsMM = map.getAnimalsMM();
+        List<Animal> strongestAnimals = new LinkedList<>();
+        int strongestEnergy = 0;
+        for (Animal animal : animalsMM.get(position)) {
+            strongestEnergy = max(strongestEnergy, animal.getEnergy());
+        }
+        for (Animal animal : animalsMM.get(position)) {
+            if (animal.getEnergy() == strongestEnergy) {
+                strongestAnimals.add(animal);
+            }
+        }
+        return strongestAnimals;
+    }
+
+    public void eatGrass(){
+        Multimap<Vector2d, Animal> animalsMM = map.getAnimalsMM();
+        Map<Vector2d, Grass> grassTilesHM = map.getGrassTilesHM();
+        Vector2d[] grassTiles = grassTilesHM.keySet().toArray(new Vector2d[0]);
+
+        for(Vector2d grassPosition : grassTiles){
+            if (animalsMM.get(grassPosition).size() >= 1){
+                grassTilesHM.remove(grassPosition);
+                List<Animal> strongestAnimals = getStrongestAnimals(grassPosition);
+                int grassEnergyValueSplit = grassEnergyValue/strongestAnimals.size();
+                for(Animal animal : strongestAnimals){
+                    animal.addEnergy(grassEnergyValueSplit);
+                }
+            }
+        }
+    }
+
+    public void removeDead() {
+        Multimap<Vector2d, Animal> animalsMM = map.getAnimalsMM();
+        Collection<Animal> animalCollection = animalsMM.values();
+        Animal[] animals = new Animal[animalCollection.size()];
+        animals = animalCollection.toArray(animals);
+
+        for(Animal animal : animals){
+            if(animal.getEnergy() <= 0){
+                deadAnimals.add(animal);
+                animal.removeAllObservers();
+                animalsMM.remove(animal.getPosition(), animal);
+            }
+        }
+    }
+
+    public void reproduceIfPossible(){
+        Multimap<Vector2d, Animal> animalsMM = map.getAnimalsMM();
+
+        Vector2d[] animalPositions = animalsMM.keySet().toArray(new Vector2d[0]);
+        for (Vector2d position : animalPositions){
+            if (animalsMM.get(position).size() >= 2){
+                Animal strongest1 = null;
+                Animal strongest2 = null;
+                for (Animal animal : animalsMM.get(position)){
+                    if (strongest1 == null){
+                        strongest1 = animal;
+                    }
+                    else if (strongest2 == null){
+                        if (strongest1.getEnergy() > animal.getEnergy()){
+                            strongest2 = animal;
+                        }
+                        else{
+                            strongest2 = strongest1;
+                            strongest1 = animal;
+                        }
+                    }
+                    else if (strongest1.getEnergy() < animal.getEnergy()){
+                        strongest2 = strongest1;
+                        strongest1 = animal;
+                    }
+                    else if (strongest2.getEnergy() < animal.getEnergy()){
+                        strongest2 = animal;
+                    }
+                }
+                if (strongest1.getEnergy() > startingEnergy/2 && strongest2.getEnergy() > startingEnergy/2){
+                    int childEnergy = strongest1.getEnergy()/2 + strongest2.getEnergy()/2;
+                    strongest1.setEnergy(strongest1.getEnergy()/2);
+                    strongest2.setEnergy(strongest2.getEnergy()/2);
+                    strongest1.increaseChildNo();
+                    map.place(new Animal(this.map, this.map.findFreePositionForChild(position), strongest1.getGenes(),strongest2.getGenes(), childEnergy));
+                }
+            }
+        }
+    }
+
 }
